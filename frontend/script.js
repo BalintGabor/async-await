ac1102ff85a01f210b35163ee84b04cc1daaea84
Gabor Balint <trondheim.bg@gmail.com>
2022-05-16 17:39:45 +0200

    1st day

--- frontend/script.js
+++ frontend/script.js
@@ -20,11 +20,19 @@ const loadEvent = async function () {
             <img src="${responseJson.hdurl}">
         `)*/
 
-    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=c07efd10da9e4134a49130617221605&q=Budapest&aqi=no")
+
+    
+    let chosencity = "London"
+    let url = "http://api.weatherapi.com/v1/current.json?key=c07efd10da9e4134a49130617221605&q="+ chosencity +"&aqi=no"
+
+    const response = await fetch(url)
     const responseJson = await response.json()
     console.log(responseJson);
     rootElement.insertAdjacentHTML("beforeend", `
-            <div>${response.json.location.name}"bármi"</div>
+            <div>Helység: ${responseJson.location.name}</div>
+            <div>Hőmérséklet: ${responseJson.current.temp_c}°C</div>
+            <div>Páratartalom: ${responseJson.current.humidity}%</div>
+            <div>Felhőzet mértéke: ${responseJson.current.cloud}%</div>
         `)
 }
 window.addEventListener("load", loadEvent)
