7f88bc9975f9c91d4821b2d114aa9b268452e255
Gabor Balint <trondheim.bg@gmail.com>
2022-05-16 16:07:15 +0200

    group exc

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,31 @@
-const loadEvent = function () {
-    
+const loadEvent = async function () {
+    const rootElement = document.getElementById("root")
+
+    /*fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY").then(function (response){
+        console.log(response);
+        return response.json()
+    })
+    .then(function(json){
+        console.log(json.date);
+        rootElement.insertAdjacentHTML("beforeend", `
+            <img src="${json.hdurl}">
+        `)
+    })*/
+
+    /*const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+
+    const responseJson = await response.json()
+
+    rootElement.insertAdjacentHTML("beforeend", `
+            <img src="${responseJson.hdurl}">
+        `)*/
+
+    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=c07efd10da9e4134a49130617221605&q=Budapest&aqi=no")
+    const responseJson = await response.json()
+    console.log(responseJson);
+    rootElement.insertAdjacentHTML("beforeend", `
+            <div>${response.json.location.name}"bármi"</div>
+        `)
 }
 window.addEventListener("load", loadEvent)
+
