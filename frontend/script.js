662b7d0ad66d055feae17128828ae2acd0647cb4
Gabor Balint <trondheim.bg@gmail.com>
2022-05-18 15:54:05 +0200

    done till datalist

--- frontend/script.js
+++ frontend/script.js
@@ -1,42 +1,37 @@
 const loadEvent = async function () {
     const rootElement = document.getElementById("root")
-
-    let output = "pilisv"
-    let listOfCitiesUrl = "http://api.weatherapi.com/v1/search.json?key=c07efd10da9e4134a49130617221605&q=" + output;
-
-    const response01 = await fetch(listOfCitiesUrl)
-    const response01Json = await response01.json()
-    console.log(response01Json);
-
-    /*let filtered = response01Json.filter(function(obj){
-        //loop through each object
-        for(key in obj){
-          //check if object value contains value you are looking for
-          if(obj[key].includes(output)){
-            //add this object to the filtered array
-            return obj;
-        }
+    
+    const dataListComponent = `
+    <label for="choose-a-city">Choose a city:</label>
+    <input type="text" list="chosen-cities" id="choose-a-city" name="choose-a-city">
+    <datalist id="chosen-cities">
+    </datalist>
+    `
+    rootElement.insertAdjacentHTML("beforeend", dataListComponent)
+    
+    let search = document.getElementById("choose-a-city");
+    
+    search.addEventListener("input", updateValue);
+    function updateValue () {
+        getCity(search.value)
     }
-   });
-   console.log(filtered)*/
-
-   const dataListComponent = `
-        <label for="choose-a-city">Choose a city:</label>
-        <input list="chosen-cities" id="choose-a-city" name="choose-a-city" />
-        <datalist id="chosen-cities">
-        </datalist>
-        `
+    
+    async function getCity (value) {
+        const response01 = await fetch("http://api.weatherapi.com/v1/search.json?key=c07efd10da9e4134a49130617221605&q=" + value)
+        const cities = await response01.json()
         
-        
-        const insertResultCities = function(resultCities) {
-            const citiesContainer = document.getElementById("chosen-cities")
+        insertResultCities(cities);
+    }
+ 
+    const insertResultCities = function(resultCities) {
+        const citiesContainer = document.getElementById("chosen-cities")
 
-            for (const city of resultCities) {
-                if (city.name !== null) {
-                    console.log(city.name)
-                    let cityHtml = `<option value="${city.name}">`
-                    
-                    citiesContainer.insertAdjacentHTML("beforeend", cityHtml)
+        for (const city of resultCities) {
+            if (city.name !== null) {
+                console.log(city.name)
+                let cityHtml = `<option value="${city.name}">`
+                
+                citiesContainer.insertAdjacentHTML("beforeend", cityHtml)
             }
         }
     }
@@ -54,9 +49,8 @@ const loadEvent = async function () {
             <div>Páratartalom: ${responseJson.current.humidity}%</div>
             <div>Felhőzet mértéke: ${responseJson.current.cloud}%</div>
         `)*/
-    rootElement.insertAdjacentHTML("beforeend", dataListComponent)
 
-    insertResultCities(response01Json);
+
 }
 window.addEventListener("load", loadEvent)
 
